95907adfc9c721a4f6742544e369783ab8610cd9
El-sheshtawy <ramyalfe22@gmail.com>
2025-09-29 23:34:43 +0300

    Add new FAQ categories to API endpoint

--- app/Http/Controllers/Api/FaqController.php
+++ app/Http/Controllers/Api/FaqController.php
@@ -21,6 +21,15 @@ class FaqController extends Controller
                 'background_image' => $faq->background_image ? asset('storage/' . $faq->background_image) : null,
                 'general' => $grouped['general'] ?? [],
                 'payment' => $grouped['payment'] ?? [],
+                'products_services' => $grouped['products_services'] ?? [],
+                'returns_missing_parts' => $grouped['returns_missing_parts'] ?? [],
+                'security_privacy' => $grouped['security_privacy'] ?? [],
+                'registration_account' => $grouped['registration_account'] ?? [],
+                'customer_support' => $grouped['customer_support'] ?? [],
+                'warranty' => $grouped['warranty'] ?? [],
+                'shipping' => $grouped['shipping'] ?? [],
+                'returns_refunds' => $grouped['returns_refunds'] ?? [],
+                'how_to_order' => $grouped['how_to_order'] ?? [],
             ]
         ]);
     }
