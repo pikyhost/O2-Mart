7580de438311ad66b3ec4a5ec06afdce42d6f00a
El-sheshtawy <ramyalfe22@gmail.com>
2025-10-01 01:27:37 +0300

    done

--- app/Http/Controllers/Api/TyreController.php
+++ app/Http/Controllers/Api/TyreController.php
@@ -324,58 +324,58 @@ class TyreController extends Controller
 
     public function brands()
     {
-        return response()->json(['status' => 'success', 'data' => TyreBrand::select('id', 'name')->whereHas('tyres')->get()]);
+        return response()->json(['status' => 'success', 'data' => TyreBrand::select('id', 'name')->get()]);
     }
 
     public function widths()
     {
-        $data = Tyre::select('width')->distinct()->whereNotNull('width')->pluck('width')->filter()->values();
+        $data = Tyre::select('width')->distinct()->pluck('width')->filter()->values();
         return response()->json(['status' => 'success', 'data' => $data]);
     }
 
     public function heights()
     {
-        $data = Tyre::select('height')->distinct()->whereNotNull('height')->pluck('height')->filter()->values();
+        $data = Tyre::select('height')->distinct()->pluck('height')->filter()->values();
         return response()->json(['status' => 'success', 'data' => $data]);
     }
 
     public function wheelDiameters()
     {
-        $data = Tyre::select('wheel_diameter')->distinct()->whereNotNull('wheel_diameter')->pluck('wheel_diameter')->filter()->values();
+        $data = Tyre::select('wheel_diameter')->distinct()->pluck('wheel_diameter')->filter()->values();
         return response()->json(['status' => 'success', 'data' => $data]);
     }
 
     public function models()
     {
-        return response()->json(['status' => 'success', 'data' => TyreModel::select('id', 'name')->whereHas('tyres')->get()]);
+        return response()->json(['status' => 'success', 'data' => TyreModel::select('id', 'name')->get()]);
     }
 
     public function loadIndexes()
     {
-        $data = Tyre::select('load_index')->distinct()->whereNotNull('load_index')->pluck('load_index')->filter()->values();
+        $data = Tyre::select('load_index')->distinct()->pluck('load_index')->filter()->values();
         return response()->json(['status' => 'success', 'data' => $data]);
     }
 
     public function speedRatings()
     {
-        $data = Tyre::select('speed_rating')->distinct()->whereNotNull('speed_rating')->pluck('speed_rating')->filter()->values();
+        $data = Tyre::select('speed_rating')->distinct()->pluck('speed_rating')->filter()->values();
         return response()->json(['status' => 'success', 'data' => $data]);
     }
 
     public function productionYears()
     {
-        $data = Tyre::select('production_year')->distinct()->whereNotNull('production_year')->pluck('production_year')->filter()->values();
+        $data = Tyre::select('production_year')->distinct()->pluck('production_year')->filter()->values();
         return response()->json(['status' => 'success', 'data' => $data]);
     }
 
     public function countries()
     {
-        return response()->json(['status' => 'success', 'data' => TyreCountry::select('id', 'name')->whereHas('tyres')->get()]);
+        return response()->json(['status' => 'success', 'data' => TyreCountry::select('id', 'name')->get()]);
     }
 
     public function warranties()
     {
-        $data = Tyre::select('warranty')->distinct()->whereNotNull('warranty')->pluck('warranty')->filter()->values();
+        $data = Tyre::select('warranty')->distinct()->pluck('warranty')->filter()->values();
         return response()->json(['status' => 'success', 'data' => $data]);
     }
 
@@ -439,18 +439,17 @@ class TyreController extends Controller
     {
         return response()->json([
             'status' => 'success',
-            'filters' => [
-                'widths' => Tyre::query()->select('width')->distinct()->whereNotNull('width')->pluck('width')->filter(),
-                'heights' => Tyre::query()->select('height')->distinct()->whereNotNull('height')->pluck('height')->filter(),
-                'wheel_diameters' => Tyre::query()->select('wheel_diameter')->distinct()->whereNotNull('wheel_diameter')->pluck('wheel_diameter')->filter(),
-                'load_indexes' => Tyre::query()->select('load_index')->distinct()->whereNotNull('load_index')->pluck('load_index')->filter(),
-                'speed_ratings' => Tyre::query()->select('speed_rating')->distinct()->whereNotNull('speed_rating')->pluck('speed_rating')->filter(),
-                'production_years' => Tyre::query()->select('production_year')->distinct()->whereNotNull('production_year')->pluck('production_year')->filter(),
-                'warranties' => Tyre::query()->select('warranty')->distinct()->whereNotNull('warranty')->pluck('warranty')->filter(),
-                'brands' => TyreBrand::select('id', 'name')->whereHas('tyres')->get(),
-                'models' => TyreModel::select('id', 'name')->whereHas('tyres')->get(),
-                'countries' => TyreCountry::select('id', 'name')->whereHas('tyres')->get(),
-                'sizes' => \App\Models\TyreSize::select('id', 'size')->whereHas('tyres')->get(),
+            'data' => [
+                'brands' => \App\Models\TyreBrand::select('id', 'name')->get(),
+                'widths' => Tyre::select('width')->distinct()->pluck('width')->filter()->values(),
+                'heights' => Tyre::select('height')->distinct()->pluck('height')->filter()->values(),
+                'wheel_diameters' => Tyre::select('wheel_diameter')->distinct()->pluck('wheel_diameter')->filter()->values(),
+                'models' => \App\Models\TyreModel::select('id', 'name')->get(),
+                'load_indexes' => Tyre::select('load_index')->distinct()->pluck('load_index')->filter()->values(),
+                'speed_ratings' => Tyre::select('speed_rating')->distinct()->pluck('speed_rating')->filter()->values(),
+                'production_years' => Tyre::select('production_year')->distinct()->pluck('production_year')->filter()->values(),
+                'countries' => \App\Models\TyreCountry::select('id', 'name')->get(),
+                'warranties' => Tyre::select('warranty')->distinct()->pluck('warranty')->filter()->values(),
             ],
         ]);
     }
