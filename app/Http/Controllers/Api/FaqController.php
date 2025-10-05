65d554ed2fe4482e803235219bcd8438bc0e12bd
El-sheshtawy <ramyalfe22@gmail.com>
2025-10-05 21:17:45 +0300

    done

--- app/Http/Controllers/Api/FaqController.php
+++ app/Http/Controllers/Api/FaqController.php
@@ -13,32 +13,18 @@ class FaqController extends Controller
 
         $items = $faq->items ?? [];
 
-        $grouped = collect($items)->groupBy('category')->toArray();
-        
-        foreach ($grouped as $key => $items) {
-            $grouped[$key] = array_map(function($item) {
-                if (isset($item['category'])) {
-                    $item['category'] = ucwords(str_replace('_', ' ', $item['category']));
-                }
-                return $item;
-            }, $items);
-        }
+        $grouped = collect($items)->groupBy('category')->map(function($items, $category) {
+            return [
+                'category_name' => ucwords(str_replace('_', ' ', $category)),
+                'items' => $items
+            ];
+        })->toArray();
 
         return response()->json([
             'status' => 'success',
             'data' => [
                 'background_image' => $faq->background_image ? asset('storage/' . $faq->background_image) : null,
-                'general' => $grouped['general'] ?? [],
-                'payment' => $grouped['payment'] ?? [],
-                'products_services' => $grouped['products_services'] ?? [],
-                'returns_missing_parts' => $grouped['returns_missing_parts'] ?? [],
-                'security_privacy' => $grouped['security_privacy'] ?? [],
-                'registration_account' => $grouped['registration_account'] ?? [],
-                'customer_support' => $grouped['customer_support'] ?? [],
-                'warranty' => $grouped['warranty'] ?? [],
-                'shipping' => $grouped['shipping'] ?? [],
-                'returns_refunds' => $grouped['returns_refunds'] ?? [],
-                'how_to_order' => $grouped['how_to_order'] ?? [],
+                'categories' => $grouped
             ]
         ]);
     }
