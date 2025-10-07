604e8a95ecff402bb50179baf28dda97ac8441df
El-sheshtawy <ramyalfe22@gmail.com>
2025-10-08 02:16:00 +0300

    Add image processing to markdown in HomeController

--- app/Http/Controllers/Api/HomeController.php
+++ app/Http/Controllers/Api/HomeController.php
@@ -90,21 +90,11 @@ class HomeController extends Controller
 
 private function processInlineMarkdown($text)
 {
-    // Images ![alt](url)
     $text = preg_replace('/!\[([^\]]*)\]\(([^\)]+)\)/', '<img src="$2" alt="$1" style="max-width: 100%; height: auto;" />', $text);
-    
-    // Links [text](url)
     $text = preg_replace('/\[([^\]]+)\]\(([^\)]+)\)/', '<a href="$2" target="_blank">$1</a>', $text);
-    
-    // Bold
     $text = preg_replace('/\*\*(.*?)\*\*/', '<strong>$1</strong>', $text);
-    
-    // Italic
     $text = preg_replace('/\*(.*?)\*/', '<em>$1</em>', $text);
-    
-    // Code inline
     $text = preg_replace('/`([^`]+)`/', '<code>$1</code>', $text);
-    
     return $text;
 }
     
