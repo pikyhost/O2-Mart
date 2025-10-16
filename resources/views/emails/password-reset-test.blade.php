13ba2bef8993ba7ba94a32ae525c3d8ef02a6748
El-sheshtawy <ramyalfe22@gmail.com>
2025-10-17 00:37:32 +0300

    solve test template front

--- resources/views/emails/password-reset-test.blade.php
+++ resources/views/emails/password-reset-test.blade.php
@@ -128,7 +128,7 @@
             <tr>
                 <td style="vertical-align: middle;">
                     <a href="https://www.o2mart.net" target="_blank">
-                        <img src="https://i.ibb.co/0VXXGcfy/logo.png" alt="logo" border="0" style="width: 95px; height: auto; vertical-align: middle; margin-bottom: 3px;" />
+                        <img src="https://o2mart.net/images/logo_light.png" alt="O2Mart Logo" border="0" style="width: 95px; height: auto; vertical-align: middle; margin-bottom: 3px; display: block;" />
                     </a>
                 </td>
                 <td style="vertical-align: middle;">
@@ -141,10 +141,16 @@
         <div class="content">
             <p>Hello,</p>
             <p>We received a request to reset the password for your O2Mart account.</p>
-            <table role="presentation" style="margin: 20px 0;">
+            <table role="presentation" cellpadding="0" cellspacing="0" style="margin: 20px 0;">
                 <tr>
-                    <td style="background-color: #df2020; border-radius: 5px;">
-                        <a href="{{ config('app.frontend_url') }}/reset-password/{{ $token }}?email={{ $email }}" style="background-color: #df2020; color: #ffffff; padding: 12px 30px; text-decoration: none; border-radius: 5px; display: inline-block; font-weight: bold;">Reset Password</a>
+                    <td align="center">
+                        <table role="presentation" cellpadding="0" cellspacing="0">
+                            <tr>
+                                <td style="background-color: #df2020; border-radius: 5px; padding: 12px 30px;">
+                                    <a href="{{ config('app.frontend_url') }}/reset-password/{{ $token }}?email={{ $email }}" style="color: #ffffff; text-decoration: none; font-weight: bold; display: block;">Reset Password</a>
+                                </td>
+                            </tr>
+                        </table>
                     </td>
                 </tr>
             </table>
@@ -162,24 +168,28 @@
             <tr>
                 <td>
                     <p style="font-weight: bold; margin-bottom: 10px;">Follow us:</p>
-                    <div class="social-links" style="text-align: center; margin: 15px 0;">
-                        <!-- LinkedIn -->
-                        <a href="https://www.linkedin.com/company/o2mart/" target="_blank" style="display: inline-block; text-decoration: none; margin: 0 7px;">
-                            <img src="https://i.ibb.co/bjRrfjKJ/icons8-linkedin-24.png" alt="LinkedIn" style="width: 20px; height: 20px; display: inline-block; vertical-align: middle;" />
-                        </a>
-                        <!-- Facebook -->
-                        <a href="https://www.facebook.com/o2mart" target="_blank" style="display: inline-block; text-decoration: none; margin: 0 7px;">
-                            <img src="https://i.ibb.co/nNcJpzQw/icons8-facebook-logo-50.png" alt="Facebook" style="width: 20px; height: 20px; display: inline-block; vertical-align: middle;" />
-                        </a>
-                        <!-- Instagram -->
-                        <a href="https://www.instagram.com/o2mart/" target="_blank" style="display: inline-block; text-decoration: none; margin: 0 7px;">
-                            <img src="https://i.ibb.co/F4xXdtXp/icons8-instagram-logo-50-1.png" alt="Instagram" style="width: 20px; height: 20px; display: inline-block; vertical-align: middle;" />
-                        </a>
-                        <!-- WhatsApp -->
-                        <a href="https://wa.me/971561787270" target="_blank" style="display: inline-block; text-decoration: none; margin: 0 7px;">
-                            <img src="https://i.ibb.co/20ZJMSmx/icons8-whatsapp-50.png" alt="WhatsApp" style="width: 20px; height: 20px; display: inline-block; vertical-align: middle;" />
-                        </a>
-                    </div>
+                    <table role="presentation" cellpadding="0" cellspacing="0" style="margin: 15px auto;">
+                        <tr>
+                            <td align="center">
+                                <!-- LinkedIn -->
+                                <a href="https://www.linkedin.com/company/o2mart/" target="_blank" style="display: inline-block; margin: 0 5px;">
+                                    <img src="https://cdn-icons-png.flaticon.com/512/174/174857.png" alt="LinkedIn" width="20" height="20" style="display: block;" />
+                                </a>
+                                <!-- Facebook -->
+                                <a href="https://www.facebook.com/o2mart" target="_blank" style="display: inline-block; margin: 0 5px;">
+                                    <img src="https://cdn-icons-png.flaticon.com/512/124/124010.png" alt="Facebook" width="20" height="20" style="display: block;" />
+                                </a>
+                                <!-- Instagram -->
+                                <a href="https://www.instagram.com/o2mart/" target="_blank" style="display: inline-block; margin: 0 5px;">
+                                    <img src="https://cdn-icons-png.flaticon.com/512/174/174855.png" alt="Instagram" width="20" height="20" style="display: block;" />
+                                </a>
+                                <!-- WhatsApp -->
+                                <a href="https://wa.me/971561787270" target="_blank" style="display: inline-block; margin: 0 5px;">
+                                    <img src="https://cdn-icons-png.flaticon.com/512/124/124034.png" alt="WhatsApp" width="20" height="20" style="display: block;" />
+                                </a>
+                            </td>
+                        </tr>
+                    </table>
                     <p style="margin-top: 10px;">&copy; {{ date('Y') }} O2Mart. All rights reserved.</p>
                 </td>
             </tr>
