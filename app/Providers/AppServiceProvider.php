2b51a0dde19078a20ec3e49cf186b25c3582ccb9
El-sheshtawy <ramyalfe22@gmail.com>
2025-10-24 16:49:04 +0300

    disable the ratelimt

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -29,7 +29,7 @@ class AppServiceProvider extends ServiceProvider
     public function boot(): void
     {
         // // Configure Rate Limiters
-        // $this->configureRateLimiting();
+        $this->configureRateLimiting();
         
         // Set password validation rules
         Password::defaults(function () {
